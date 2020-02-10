b043989b70dd431fc46ee9dfc169c850a3b277c2
Chancie Johnson <chancie.johnson@enhanceit.us>
2020-02-10 02:34:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsamplenew/activity_new_word.kt
@@ -0,0 +1,12 @@
+package com.example.roomwordsamplenew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_new_word : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
